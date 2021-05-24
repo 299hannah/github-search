3b984f7b501215065eef55d89f95c57c156f7b51
hannah <hannahmwangiwambui@gmail.com>
2021-05-24 03:06:59 +0300

    user fetch data

--- src/app/gitdetails/gitdetails.component.ts
+++ src/app/gitdetails/gitdetails.component.ts
@@ -1,29 +1,30 @@
-// import { Component, OnInit } from '@angular/core';
-// import { GithubserviceService } from '../githubservice.service';
-// import { User } from '../user';
+import { Component, OnInit } from '@angular/core';
+import { GithubserviceService } from '../githubservice.service';
+import { User } from '../user';
 
 
-// @Component({
-//   selector: 'app-gitdetails',
-//   templateUrl: './gitdetails.component.html',
-//   styleUrls: ['./gitdetails.component.css']
-// })
-// export class GitdetailsComponent implements OnInit {
-//   user:User;
-//   repoDetails = [];
-//   githubserviceService:GithubserviceService
+@Component({
+  selector: 'app-gitdetails',
+  templateUrl: './gitdetails.component.html',
+  styleUrls: ['./gitdetails.component.css']
+})
+export class GitdetailsComponent implements OnInit {
+  user:User;
+  repoDetails = [];
+  githubserviceService:GithubserviceService
 
-//   constructor(  githubserviceService:GithubserviceService
-//     ) {
-//   this.githubserviceService=GithubserviceService
+  constructor(  githubserviceService:GithubserviceService
+    ) {
+  this.githubserviceService=githubserviceService
 
-//      }
+     }
 
-//   ngOnInit(): void {
-//     this.user = this.githubserviceService.user;
+  ngOnInit(){
+    this.user = this.githubserviceService.user;
+    this.repoDetails = this.githubserviceService.repoData;
     
-//   }
+  }
 
-// }
+}
 
 
--- src/app/githubservice.service.ts
+++ src/app/githubservice.service.ts
@@ -1,11 +1,55 @@
 import { Injectable } from '@angular/core';
-import { from } from 'rxjs';
-import { GithubserviceService } from ''
+import { HttpClient } from '@angular/common/http';
+
+import { User } from './user';
 @Injectable({
   providedIn: 'root'
 })
 export class GithubserviceService {
-  user: import("/home/annah/Documents/core/github-search/src/app/user").User;
+  
+  user: User;
+  repoData = [];
+  newUserData: any = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.user = new User("", 0, "", "", new Date());
+
+
+  }
+  getUserData(username: string) {
+    interface ApiResponse {
+      bio: string,
+      public_repos: number,
+      login: string,
+      avatar_url: string,
+      created_at: Date
+    }
+    let promise = new Promise<void>((resolve, reject) => {
+      this.http.get<ApiResponse>('https://api.github.com/users/' + username).toPromise().then(response => {
+        this.user.bio = response.bio;
+        this.user.public_repos = response.public_repos;
+        this.user.avatar_url = response.avatar_url;
+        this.user.created_at = response.created_at;
+
+        resolve()
+      },
+        error => {
+          reject(error)
+
+        })
+      // this.http.get<any>('https://api.github.com/users/' + username + "/repos").toPromise().then(response => {
+      //   for (let i = 0; i<response.length; i++) {
+      //     this.newUserData = new Repository(response[i].name, response[i].description, response[i].updated_at, response[i].clone_url, response[i].language)
+      //     this.repoData.push(this.newUserData);
+      //   }
+      //     resolve()
+      // },
+      //   error => {
+      //     reject(error)
+
+      //   })
+      //   })
+        // return promise;
+    })
+  }
 }
