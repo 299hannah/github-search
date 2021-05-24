43f4017da4d20a1d8b4d681f4289868199fb57e0
hannah <hannahmwangiwambui@gmail.com>
2021-05-24 20:54:57 +0300

    routing enabled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
-import { GitdetailsComponent } from './gitdetails/gitdetails.component';
+import { GitDetailsComponent } from './gitdetails/gitdetails.component';
 import { GitformComponent } from './gitform/gitform.component';
 
 
 const routes: Routes = [
-  {path: 'gitdetails', component: GitdetailsComponent},
+  {path: 'gitdetails', component: GitDetailsComponent},
   {path: 'gitform', component: GitformComponent},
   {path: 'about', component: AboutComponent},
 
